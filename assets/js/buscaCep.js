6e940524c4044a80ea6aed5eb368d61e4d33263b
Luiz Fernando <luizfvidal13@hotmail.com>
2023-08-08 20:11:30 -0300

    comentarios no arquivo Ajax

--- assets/js/buscaCep.js
+++ assets/js/buscaCep.js
@@ -1,29 +1,37 @@
+//* seleciona a div para mostrar o resultado na tela
 const resultado = document.getElementById('resultado');
 
+
+//* seleciona o formulario com os inputs de entrada
 const form = document.getElementById('formRua');
 
+
+//* função que captura o evento e impede que a aplicação recarregue a página automaticamente
 form.addEventListener('submit', function(event){
 
   event.preventDefault();
 
-  consultarCepPorRua();
 });
 
 
-//* Consultar cep por nome da rua
+//* Função para consultar o cep fornecendo o nome da rua, cidade e estado
 function consultarCepPorRua() {
    const rua = document.getElementById('rua').value;
    const cidade = document.getElementById('cidade').value;
    const uf = document.getElementById('estado').value.toUpperCase();
 
 
+  //* URL para a API ViaCep dos correios
   const url = `https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`;
 
 
+  //* Cria um novo objeto XMLHttpRequest 
   let xmlHttp = new XMLHttpRequest();
 
+  //* Abre uma conexão GET assíncrona com a URL da API
   xmlHttp.open('GET', url, true);
 
+  //* Define uma função a ser chamada quando o estado da requisição mudar
   xmlHttp.onreadystatechange = () => {
 
     if(xmlHttp.readyState === 4 && xmlHttp.status === 200){
@@ -31,16 +39,18 @@ function consultarCepPorRua() {
       let dadosText = xmlHttp.responseText;
       let dadosObj = JSON.parse(dadosText);  
       
+
+      //* Limpa o conteudo anterior da div resultado
       resultado.innerHTML = '';
 
+      //* Cria um novo elemento de div para os resultados
       const divResultado = document.createElement('div');
       divResultado.id = 'resultadoCep';
 
 
       if(!dadosObj.erro){
 
-        console.log(dadosObj);
-
+        //* Mapeia os CEPs e complementos a partir dos dados da API
         const cepsEncontrados = dadosObj.map(obj => obj.cep);
         const complementos = dadosObj.map(obj => obj.complemento);
 
@@ -51,28 +61,30 @@ function consultarCepPorRua() {
           document.getElementById('rua').value = '';
           document.getElementById('cidade').value = '';
 
+          //* Faz o loop para exibir cada CEP com seu complemento
           for(let i = 0; i < cepsEncontrados.length; i++){
             divResultado.innerHTML += `<p><span></span>${cepsEncontrados[i]} - ${complementos[i]}</p>`;
           }
         }
         
       }else{
-
+        //* Exibe uma mensagem de erro caso ocorra um erro na consulta
         divResultado.innerHTML += '<p><span>Error: </span> Ocorreu um erro na consulta!</p>';
         console.error('Ocorreu um erro na consulta! <br> Status:', xmlHttp.status);
 
       }
 
-      resultado.appendChild(divResultado); 
-      
+      //* Adiciona a div de resultados á div de resultado principal
+      resultado.appendChild(divResultado);    
 
     }
   
   }
 
+  //* Envia a requisção
   xmlHttp.send();
 
 
-  }
+}
 
 
--- assets/js/buscaEndereco.js
+++ assets/js/buscaEndereco.js
@@ -1,17 +1,24 @@
-
+//* seleciona a div para mostrar o resultado na tela
 const resultado = document.getElementById('resultado');
+
+//* Seleciona o input para capturar o valor digitado
 const cep = document.getElementById('cepInput').value.replace('-', '');
 
+
+//* Função para consultar o endereço fornecendo o numero do CEP
 function getDadosCEP(cep){
 
+    //* URL para a API ViaCep dos correios
     let url = 'https://viacep.com.br/ws/'+cep+'/json/';
 
-    //console.log(url);
 
+    //* Cria um novo objeto XMLHttpRequest 
     let xmlHttp = new XMLHttpRequest()
 
+    //* Abre uma conexão GET assíncrona com a URL da API
     xmlHttp.open('get', url)
 
+    //* Define uma função a ser chamada quando o estado da requisição mudar
     xmlHttp.onreadystatechange = () => {
 
         if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
@@ -19,15 +26,20 @@ function getDadosCEP(cep){
             let dadosText =  xmlHttp.responseText;
             let dadosObj = JSON.parse(dadosText);
 
+            //* Limpa o conteudo anterior da div resultado
             resultado.innerHTML = '';
 
+            //* Cria um novo elemento de div para os resultados
             const divResultadoCep = document.createElement('div');
             divResultadoCep.id = 'resultadoEnd';
 
+            //* Cria um novo elemento de div para o titulo
             const titulo = document.createElement('span');
             titulo.textContent = 'RESULTADOS DA SUA BUSCA!';
             divResultadoCep.appendChild(titulo);
 
+
+            //* Verifica se os dados são válidos e imprime na tela
             if(dadosObj.cep){
                 divResultadoCep.innerHTML += '<p><span>Cep: </span>' +dadosObj.cep+'</p>';
             }else{
@@ -64,13 +76,17 @@ function getDadosCEP(cep){
                 divResultadoCep.innerHTML += '<p><span>DDD: </span>Sem resultados!</p>'
             }
             
+            //* Adiciona a div de resultados á div de resultado principal
             resultado.appendChild(divResultadoCep);           
         }
 
     }
 
+     //* Envia a requisção
      xmlHttp.send();
 }
+
+
 
 //*Função para aplicar uma máscara ao input do CEP
 function mascararCep(input){
